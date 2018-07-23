9f19d21719b03a8a6b2ad5150983ae23e7beb84f
duncan-iaria <duncaniaria@gmail.com>
2018-07-22 20:07:11 -0400

    ✨ added boolean for welcome dinner, updateGuest now returns updated guest (not original doc)

--- Utils/index.js
+++ Utils/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getConfigOptions: () => {
-    const DB_URI = process.env.ENV === 'DEV' ? process.env.DB_DEV_URI : process.env.DB_PROD_URI;
-    const DB_NAME = process.env.ENV === 'DEV' ? process.env.DB_DEV_NAME : process.env.DB_PROD_NAME;
+    const DB_URI = process.env.ENV === 'PROD' ? process.env.DB_PROD_URI : process.env.DB_DEV_URI;
+    const DB_NAME = process.env.ENV === 'PROD' ? process.env.DB_PROD_NAME : process.env.DB_DEV_NAME;
     return [DB_URI, DB_NAME];
-  },
+  }
 };
